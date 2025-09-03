6fabfb11b3642b4be25f9951039d00735e5916fd
ekt15 <ekt15@gmail.com>
2025-09-03 12:43:55 +1200

    create an abstract class `Gate` to define the properties and methods that every gate will have

---
+++ Circuits/Gate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circuits
+{
+    /// <summary>
+    /// This class defines the properties and methods that every gate will have
+    /// </summary>
+    public abstract class Gate
+    {
+    }
+}
