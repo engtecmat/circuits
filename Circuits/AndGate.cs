5f8b14d313bdfe18ae18dfa5a0c3c3a44bb45db8
ekt15 <ekt15@gmail.com>
2025-09-04 09:00:57 +1200

    change `Draw` method to an abstract method in `Gate` class, the implementation will be done in subclasses

--- Circuits/AndGate.cs
+++ Circuits/AndGate.cs
@@ -1,4 +1,6 @@
-﻿namespace Circuits
+﻿using System.Drawing;
+
+namespace Circuits
 {
     /// <summary>
     /// This class implements an AND gate with two inputs
@@ -20,6 +22,37 @@
             pins.Add(new Pin(this, false, 20));
             //move the gate and the pins to the position passed in
             MoveTo(x, y);
+        }
+
+        /// <summary>
+        /// Draws the gate in the normal colour or in the selected colour.
+        /// </summary>
+        /// <param name="g"></param>
+        public override void Draw(Graphics g)
+        {
+            Brush brush;
+            //Check if the gate has been selected
+            if (selected)
+            {
+                brush = selectedBrush;
+            }
+            else
+            {
+                brush = normalBrush;
+            }
+            //Draw each of the pins
+            foreach (Pin p in pins)
+                p.Draw(g);
+
+            // AND is simple, so we can use a circle plus a rectange.
+            // An alternative would be to use a bitmap.
+            g.FillEllipse(brush, left, top, WIDTH, HEIGHT);
+            g.FillRectangle(brush, left, top, WIDTH / 2, HEIGHT);
+
+            //Note: You can also use the images that have been imported into the project if you wish,
+            //      using the code below.  You will need to space the pins out a bit more in the constructor.
+            //      There are provided images for the other gates and selected versions of the gates as well.
+            //paper.DrawImage(Properties.Resources.AndGate, Left, Top);
         }
     }
 }
